c754a0b057b00f370420dd2f660bc36dcbb5291b
volkde <ilyaprofit@gmail.com>
2024-11-04 21:57:44 +0100

    homework 38

---
+++ src/homework_38/ComparatorApp.java
@@ -0,0 +1,45 @@
+package homework_38;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class ComparatorApp {
+
+    public static void main(String[] args) {
+
+
+        Sportsman[] sportsmen = {
+
+                new Sportsman("Ivanov", 31, 85),
+                new Sportsman("Petrov", 44, 90),
+                new Sportsman("Sidorov", 23, 70),
+                new Sportsman("Kravchuk", 28, 95)
+
+        };
+
+
+        Arrays.sort(sportsmen);
+        System.out.println("Сортировка по имени (Comparable):");
+        System.out.println(Arrays.toString(sportsmen));
+
+
+        Arrays.sort(sportsmen, new SportsmanScore());
+        System.out.println("Сортировка по рейтингу (Comparator):");
+        System.out.println(Arrays.toString(sportsmen));
+
+
+        Arrays.sort(sportsmen, new Comparator<Sportsman>() {
+
+            @Override
+            public int compare(Sportsman s1, Sportsman s2) {
+
+                return Integer.compare(s1.getAge(), s2.getAge());
+
+            }
+
+        });
+
+        System.out.println("Сортировка по возрасту (анонимный класс Comparator):");
+        System.out.println(Arrays.toString(sportsmen));
+    }
+}
---
+++ src/lesson_38/CarModelComparator.java
@@ -0,0 +1,17 @@
+package lesson_38;
+
+import java.util.Comparator;
+
+
+public class CarModelComparator implements Comparator<Car> {
+    @Override
+    public int compare(Car car1, Car car2) {
+        // String, Integer и все оберточные классы - реализуют интерфейс Comparable
+        String model1 = car1.getModel();
+        String model2 = car2.getModel();
+
+        return model1.compareTo(model2);
+
+        // String реализует по умолчанию лексикографическое сравнение строк
+    }
+}
