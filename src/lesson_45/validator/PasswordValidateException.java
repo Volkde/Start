5eca64f500c54a4dc00aa74a93191800d77380c7
volkde <ilyaprofit@gmail.com>
2024-11-13 18:30:22 +0100

    homework 45

---
+++ src/lesson_45/validator/PasswordValidateException.java
@@ -0,0 +1,22 @@
+package lesson_45.validator;
+
+
+public class PasswordValidateException extends Exception {
+    /*
+    Если я хочу создать исключение, достаточно наследоваться от одного из двух классов:
+    1. Проверяемое исключение - Exception
+    2. Не проверяемое - RuntimeException
+
+    getMessage() - возвращает строку с коротким описание исключения
+     */
+
+  public PasswordValidateException(String message) {
+    super(message);
+  }
+
+  @Override
+  public String getMessage() {
+    return "Password validate exception | " + super.getMessage();
+  }
+}
+
